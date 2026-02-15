aa9961216c95287dd4181f33c6e3b13d36f8fc60
Kunal <kunalpatel969@gmail.com>
2026-02-15 22:38:00 +0530

    New Update

---
+++ app/src/main/java/com/torque/patel/basicscience/Custom_Radios.kt
@@ -0,0 +1,21 @@
+package com.torque.patel.basicscience
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Custom_Radios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_custom_radios)
+
+    }
+    fun Mymenu(){
+        val one:Int = 1
+        val two:Int = 2
+        val three:Int = 3
+    }
+}
